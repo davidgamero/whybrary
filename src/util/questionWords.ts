0ee5d4152bfea09eb17eaa76c5064ec00d1700e1
David Gamero <david340804@gmail.com>
2021-01-11 22:29:15 -0500

    add Sets for stopwords and questionwords

---
+++ src/util/questionWords.ts
@@ -0,0 +1,20 @@
+let questionWordsArray: string[] = [
+    `are`,
+    `do`,
+    `how`,
+    `what`,
+    `when`,
+    `where`,
+    `which`,
+    `who`,
+    `whose`,
+    `why`
+]
+
+let questionWords = new Set();
+
+questionWordsArray.forEach((word) => {
+    questionWords.add(word);
+})
+
+export default questionWords;
